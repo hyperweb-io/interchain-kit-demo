67fd0e6192060128aa5a001ce920aac6c695cc39
Eason Smith <eason@smi.th>
2024-11-08 15:58:47 +0800

    using osmosis testnet

--- config/defaults.ts
+++ config/defaults.ts
@@ -1,7 +1,7 @@
 import { assetLists, chains } from "@chain-registry/v2";
 
-export const defaultChainName = 'cosmoshub'
-export const defaultRpcEndpoint = 'https://cosmos-rpc.publicnode.com'
+export const defaultChainName = 'osmosistestnet' // 'cosmoshub'
+export const defaultRpcEndpoint = 'https://rpc.testnet.osmosis.zone' // 'https://cosmos-rpc.publicnode.com'
 
 export const defaultChain = chains.find((chain) => chain.chainName === defaultChainName)
 
