fb2fa3f2e58d60df89dccc7f9c1450d4a9ac791c
Eason Smith <eason@smi.th>
2024-11-13 10:42:46 +0800

    show balance and result

--- pages/ethers/index.tsx
+++ pages/ethers/index.tsx
@@ -1,8 +1,12 @@
 import { Box, Button, Text } from "@interchain-ui/react";
 import { ethers } from "ethers";
+import { useState } from "react";
 
 export default function Index() {
+  const [balance, setBalance] = useState('--')
+  const [transactionReceipt, setTransactionReceipt] = useState<ethers.TransactionReceipt | null>(null)
   const send = async () => {
+    setTransactionReceipt(null)
     if (!window.ethereum) {
       alert('Please install MetaMask')
       return
@@ -14,10 +18,23 @@ export default function Index() {
       value: '1'
     })
     const res = await tx.wait();
-    alert('Transaction sent, see console for details')
-    console.log(res)
+    setTransactionReceipt(res)
+    getBalance()
+  }
+
+  const getBalance = async () => {
+    if (!window.ethereum) return
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const wallet = await provider.getSigner()
+    const balance = await provider.getBalance(wallet.getAddress())
+    setBalance(ethers.formatEther(balance))
   }
   return <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
+    <Box display="flex" flexDirection="row" alignItems="center" justifyContent="center" marginBottom="1rem">
+      <Text attributes={{marginRight: '1rem'}}>Balance: {balance}</Text>
+      <Button onClick={getBalance} size="sm">Fetch</Button>
+    </Box>
     <Button onClick={send}>Send</Button>
+    {transactionReceipt && <Text attributes={{whiteSpace: 'pre-line', padding: '2rem'}} as="div">{JSON.stringify(transactionReceipt, null, 2)}</Text>}
   </Box>
 }
