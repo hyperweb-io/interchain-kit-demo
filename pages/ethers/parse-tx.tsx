c29adaebb674158fa9af89b83f8cb45a614f4c28
Eason Smith <eason@smi.th>
2024-11-28 11:18:33 +0800

    decoded smart contract data

--- pages/ethers/parse-tx.tsx
+++ pages/ethers/parse-tx.tsx
@@ -1,10 +1,20 @@
-import { Transaction } from "ethers";
-import { TextField, Box, Text, Button } from "@interchain-ui/react";
-import { useState } from "react";
+import { Transaction, Interface } from "ethers";
+import { Box, Text, Button } from "@interchain-ui/react";
+import { useEffect, useState } from "react";
+import abi from './abi.json'
+const JSONbig = require('json-bigint')
 
 export default function ParseTx() {
+  const iface = new Interface(abi)
+
   const [rawTx, setRawTx] = useState("");
   const [decodedTx, setDecodedTx] = useState<Transaction | null>(null);
+  const [dataDecoded, setDataDecoded] = useState<object | null>(null)
+
+  useEffect(()=>{
+    if (!decodedTx) return
+    setDataDecoded({dataDecoded:iface.parseTransaction({ data: decodedTx.data })})
+  }, [decodedTx])
   
   return (
 <Box display='flex' padding='20px' flexDirection='column' alignItems='center' justifyContent='center' minHeight='100vh'>
@@ -24,6 +34,10 @@ export default function ParseTx() {
     >Parse</Button>
     {decodedTx&&<Text attributes={{whiteSpace: 'pre-line', padding: '2rem'}} as="div">
       {JSON.stringify(decodedTx, null, 2)}
+    </Text>}
+
+    {dataDecoded&&<Text attributes={{whiteSpace: 'pre-line', padding: '2rem'}} as="div">
+      {JSONbig.stringify(dataDecoded, null, 2)}
     </Text>}
 </Box>
 )
