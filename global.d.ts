eb9f2199b3eae35006592e9cf1fba562e140d167
Eason Smith <eason@smi.th>
2024-12-03 10:24:34 +0800

    add @keplr-wallet/types

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,11 @@
 export {};
 
 import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Keplr } from "@keplr-wallet/types";
 
 declare global {
   interface Window {
     ethereum?: MetaMaskInpageProvider;
+    keplr?: Keplr;
   }
 }
